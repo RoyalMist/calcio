d4b4fcffd0020655c4602b006090c4012ba52538
Thibault Fouache <tfouache@pictet.com>
2021-09-29 17:52:57 +0200

    Admin creation

--- server/security/admin.go
+++ server/security/admin.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	"calcio/ent"
+	"github.com/google/uuid"
 )
 
 func CreateAdmin(client *ent.Client) (err error) {
 	ctx := NewContext(context.Background(), Claims{
-		UserId:  "admin",
+		UserId:  uuid.NewString(),
 		IsAdmin: true,
 	})
 
