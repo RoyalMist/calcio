cf45ba9fc57a87467b190379e91f9dd3869ecce4
RoyalMist <thibault.fouache@gmail.com>
2022-04-03 17:57:06 +0200

    Auth

--- web/src/stores/authentication.tsx
+++ web/src/stores/authentication.tsx
@@ -1,82 +1,95 @@
 import * as React from "react";
-import {useReducer} from "react";
-import {parse, Paseto} from "../utils/paseto";
+import { useReducer } from "react";
+import { parse, Paseto } from "../utils/paseto";
 
 enum AuthActionKind {
-    SET,
-    CLEAR,
+  SET,
+  CLEAR,
 }
 
 interface AuthAction {
-    type: AuthActionKind;
-    token?: string;
+  type: AuthActionKind;
+  token?: string;
 }
 
 interface AuthState {
-    token: string | null;
-    paseto: Paseto | null;
+  token: string | null;
+  paseto: Paseto | null;
 }
 
 interface AuthProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 interface AuthContext {
-    token: string;
-    paseto: Paseto | null;
-    isLoggedIn: () => boolean;
-    isAdmin: () => boolean;
-    dispatcher: React.Dispatch<AuthAction>
+  token: string;
+  paseto: Paseto | null;
+  isLoggedIn: () => boolean;
+  isAdmin: () => boolean;
+  dispatcher: React.Dispatch<AuthAction>;
 }
 
-const EMPTY: AuthState = {token: null, paseto: null}
-const AuthContext = React.createContext<AuthContext>({token: "", paseto: null, isLoggedIn: () => false, isAdmin: () => false, dispatcher: () => EMPTY});
+const EMPTY: AuthState = { token: null, paseto: null };
+const AuthContext = React.createContext<AuthContext>({
+  token: "",
+  paseto: null,
+  isLoggedIn: () => false,
+  isAdmin: () => false,
+  dispatcher: () => EMPTY,
+});
 const stateFromToken = (token: string | null): AuthState => {
-    const paseto = parse(token);
-    if (token != null && paseto != null && Date.parse(paseto.exp) > Date.now()) {
-        localStorage.setItem("token", token);
-        return {token, paseto};
-    } else {
-        localStorage.removeItem("token");
-        return EMPTY;
+  const paseto = parse(token);
+  if (token != null && paseto != null && Date.parse(paseto.exp) > Date.now()) {
+    localStorage.setItem("token", token);
+    return { token, paseto };
+  } else {
+    localStorage.removeItem("token");
+    return EMPTY;
+  }
+};
+
+const authReducer = (_: AuthState, action: AuthAction): AuthState => {
+  const { type, token } = action;
+  switch (type) {
+    case AuthActionKind.SET: {
+      return stateFromToken(token === undefined ? null : token);
     }
-}
 
-const authReducer = (state: AuthState, action: AuthAction): AuthState => {
-    const {type, token} = action;
-    switch (type) {
-        case AuthActionKind.SET: {
-            return stateFromToken(token === undefined ? null : token);
-        }
-
-        case AuthActionKind.CLEAR: {
-            return stateFromToken(null);
-        }
-
-        default: {
-            throw new Error(`Unknown action type: ${action.type}`);
-        }
+    case AuthActionKind.CLEAR: {
+      return stateFromToken(null);
     }
-}
 
-function AuthProvider({children}: AuthProps) {
-    const storedToken = localStorage.getItem("token");
-    const [state, dispatch] = useReducer(authReducer, stateFromToken(storedToken));
-    const isAdmin = () => !!stateFromToken(state.token).paseto?.is_admin;
-    const isLoggedIn = () => !!stateFromToken(state.token).paseto;
-    const value: AuthContext = {token: `Bearer ${state.token}`, paseto: state.paseto, dispatcher: dispatch, isAdmin, isLoggedIn};
-    return (
-        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-    );
+    default: {
+      throw new Error(`Unknown action type: ${action.type}`);
+    }
+  }
+};
+
+function AuthProvider({ children }: AuthProps) {
+  const storedToken = localStorage.getItem("token");
+  const [state, dispatch] = useReducer(
+    authReducer,
+    stateFromToken(storedToken)
+  );
+  const isAdmin = () => !!stateFromToken(state.token).paseto?.is_admin;
+  const isLoggedIn = () => !!stateFromToken(state.token).paseto;
+  const value: AuthContext = {
+    token: `Bearer ${state.token}`,
+    paseto: state.paseto,
+    dispatcher: dispatch,
+    isAdmin,
+    isLoggedIn,
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
 function useAuth() {
-    const context = React.useContext(AuthContext);
-    if (context === undefined) {
-        throw new Error("useAuth must be used within a AuthProvider");
-    }
+  const context = React.useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within a AuthProvider");
+  }
 
-    return context;
+  return context;
 }
 
-export {AuthActionKind, AuthProvider, useAuth};
+export { AuthActionKind, AuthProvider, useAuth };
